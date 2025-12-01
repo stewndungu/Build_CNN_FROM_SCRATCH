4ff1945a87d60cba1e13ef407e9ce11423a43ff2
stewndungu <110838587+stewndungu@users.noreply.github.com>
2025-12-01 00:25:03 +0000

    implement mse and derivative of mse

--- source/activation.cpp
+++ source/activation.cpp
@@ -47,5 +47,32 @@ double softplus(double x)
 
 double mse(const Vec& output, const Vec& target)
 {
+    double sum=0.0;
     
+    if(output.size() != target.size())
+    {
+          throw std::runtime_error("Vector dimension mismatch");
+    }
+
+    for(int i=0;i<output.size();i++)
+    {
+        double ans= target.at(i) - output.at(i);
+        sum+=(ans*ans);
+    }
+
+    return sum/static_cast<double>(output.size());
+}
+
+
+Vec mse_derivative(const Vec& output, const Vec& target)
+{
+   
+    size_t N = output.size(); 
+    Vec gradient(N);
+
+    for (size_t i = 0; i < N; ++i)
+    {
+        gradient[i] = (2.0 / static_cast<double>(N)) * (output[i] - target[i]);
+    }
+    return gradient;
 }
