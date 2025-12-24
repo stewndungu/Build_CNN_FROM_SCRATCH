c0f82c75a969b125632f487c64c077756c835348
stewndungu <110838587+stewndungu@users.noreply.github.com>
2025-12-23 20:12:19 -0600

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,8 +1,12 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
-#include <vector>
 #include "vec.hpp"
+#include <random>
+#include <sstream>
+#include <fstream>
+#include <string>
 
+using namespace std;
 using Mat = std::vector<std::vector<double>>;
 
 Mat multiply(const Mat& A, const Mat& B); 
@@ -14,6 +18,10 @@ Mat transpose (const Mat& A);
 Vec multiply(const Mat& A, const Vec& x);
 
 void print(const Mat& m);
+
+
+Mat conv2d(const Mat& img, const Mat& kernel);
+Mat load_pgm(const string& path);
 
 
 #endif
