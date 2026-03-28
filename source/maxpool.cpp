ca6475b0058e9d2c112fe0766d7b2594a2ccd9ac
stewndungu <110838587+stewndungu@users.noreply.github.com>
2026-03-28 20:58:07 +0000

    yep

--- source/maxpool.cpp
+++ source/maxpool.cpp
@@ -6,35 +6,13 @@ using namespace std;
 
 Maxpool::vector<Mat> forward(const vector<Mat>& input)
 {
-    for(int i=0;i<input.size();i++)
-    {
-        for(int row =0; row<input[i].size();row + stride)
-        {
-            for(int column = 0 ;column< input[i][row].size();column + stride)
-            {
-                double max =0;
-                int max_row=0;
-                int max_column=0;
+   //get the input dimensions
+   int num_channels = input.size();
+   this->last_input_row = input[0].size()
+   this->last_input_col = input[0][0].size();
 
-                for(int maxrow = row; maxrow < row + pool_size; maxrow++)
-                { 
-                    for(int maxcol = column; maxcol < column + pool_size;maxcol++)
-                    {
-                        if(input[i][maxrow][maxcol] > max)
-                        {
-                            max = input[i][maxrow][maxcol];
-                            max_row= maxrow;
-                            max_col=maxcol;
-                        }
-                    }
-                }
-                
-
-                // I am question why we have a 3D vector of a pair for max_coords. It seems abit odd. Why not a hash map? Or just a regular 3D matrix ?
-                maxcoords.push_back();
-            }
-        }
-    }
+   //calculate the dimensions for the output matrix
+   int out_row= this->last_input_row / str
 }
 Maxpool::vector<Mat> backward(const vector<Mat>& output_error)
 {
